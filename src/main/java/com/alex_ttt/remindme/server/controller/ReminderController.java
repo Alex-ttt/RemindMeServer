99c6183d83c2598add5cb233f719b212c5f0bac5
Alex-ttt <alexander.tarasenko93@gmail.com>
2016-11-16 14:27:11 +1000

    Added REST services

--- src/main/java/com/alex_ttt/remindme/server/controller/ReminderController.java
+++ src/main/java/com/alex_ttt/remindme/server/controller/ReminderController.java
@@ -1,10 +1,13 @@
 package com.alex_ttt.remindme.server.controller;
 
+import com.alex_ttt.remindme.server.entity.Remind;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 @RequestMapping("/reminder")
@@ -12,8 +15,17 @@ public class ReminderController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String getReminder(ModelMap model) {
-        return "My reminder";
+    public Remind getReminder(ModelMap model) {
+        return createMockRemind();
+    }
+
+    private Remind createMockRemind() {
+        Remind remind = new Remind();
+        remind.setId(1);
+        remind.setTitle("title");
+        remind.setRemindDate(new Date());
+
+        return remind;
     }
 
 }
