b6c033922a57abc1d2b233506b2bd662be1fb4ac
Alex-ttt <alexander.tarasenko93@gmail.com>
2016-11-20 22:12:49 +1000

    Fix error with versions (transactionManager exception)

--- src/main/java/com/alex_ttt/remindme/server/controller/ReminderController.java
+++ src/main/java/com/alex_ttt/remindme/server/controller/ReminderController.java
@@ -1,6 +1,8 @@
 package com.alex_ttt.remindme.server.controller;
 
 import com.alex_ttt.remindme.server.entity.Remind;
+import com.alex_ttt.remindme.server.repository.RemindRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,14 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
+import java.util.List;
 
 @Controller
 @RequestMapping("/reminder")
 public class ReminderController {
 
+    @Autowired
+    private RemindRepository remindRepository;
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
     public Remind getReminder(ModelMap model) {
+        List<Remind> all = remindRepository.findAll();
         return createMockRemind();
     }
 
